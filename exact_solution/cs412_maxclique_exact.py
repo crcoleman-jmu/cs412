5103f2a41322da76063f866c17afb91ea1718287
xXccoleXx <71529472+xXccoleXx@users.noreply.github.com>
2023-12-11 10:11:55 -0500

    Update cs412_maxclique_exact.py

--- exact_solution/cs412_maxclique_exact.py
+++ exact_solution/cs412_maxclique_exact.py
@@ -1,56 +1,38 @@
 def is_clique(graph, nodes):
-    """
-    Check if the given set of nodes forms a clique in the graph.
-    """
     for i in range(len(nodes)):
         for j in range(i + 1, len(nodes)):
-            if not graph[nodes[i]][nodes[j]]:
+            if nodes[i] not in graph or nodes[j] not in graph[nodes[i]]:
                 return False
     return True
 
-def max_clique_bruteforce(graph):
-    """
-    Brute-force approach to find the maximum clique in the graph.
-    """
-    max_clique = []
+def find_max_clique(graph, current_clique, remaining_nodes):
+    if not remaining_nodes:
+        return current_clique
 
-    # Iterate through all possible combinations of nodes
-    for i in range(1, 2**len(graph)):
-        nodes = [j for j, bit in enumerate(bin(i)[:1:-1]) if bit == '1']
-        
-        # Check if the current set of nodes forms a clique and is larger than the current max_clique
-        if is_clique(graph, nodes) and len(nodes) > len(max_clique):
-            max_clique = nodes
+    max_clique = current_clique
+    for node in remaining_nodes:
+        if all(node in graph[clique_node] for clique_node in current_clique):
+            new_clique = find_max_clique(graph, current_clique + [node], [n for n in remaining_nodes if n != node])
+            if len(new_clique) > len(max_clique):
+                max_clique = new_clique
 
     return max_clique
 
 
-
 def main():
-
     m = int(input())
-
     edge_list = [input().split() for _ in range(m)]
+    
+    # Build the graph
     graph = {}
-
     for edge in edge_list:
-
         u, v = edge
+        graph.setdefault(u, {}).setdefault(v, True)
+        graph.setdefault(v, {}).setdefault(u, True)
 
-        graph.setdefault(u, set()).add(v)
+    max_clique = find_max_clique(graph, [], list(graph.keys()))
 
-        graph.setdefault(v, set()).add(u)
-
-
-    max_clique_output = max_clique_bruteforce(graph)
-
-    print(max_clique_output)
-
+    print(" ".join(max_clique))
 
 if __name__ == "__main__":
-
     main()
-
-
-# Example usage
-
